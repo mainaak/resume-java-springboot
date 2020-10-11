1dbf005f7724b87e50ee9c4e4e1bacec17ac7939
mainaak <mainaakaanand@gmail.com>
2020-10-11 14:25:39 +0530

    init project:

---
+++ src/main/java/com/koalmine/curriculumvitae/CurriculumVitaeApplication.java
@@ -0,0 +1,13 @@
+package com.koalmine.curriculumvitae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculumVitaeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurriculumVitaeApplication.class, args);
+    }
+
+}
